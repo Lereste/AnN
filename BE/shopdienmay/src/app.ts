69a3077cd6d2eea33c4b614839c5f4a917a5555b
Lereste <hohoanghao1999@gmail.com>
2024-11-16 17:06:54 +0700

    BE: config-prettierrc

--- BE/shopdienmay/src/app.ts
+++ BE/shopdienmay/src/app.ts
@@ -1,78 +1,66 @@
-import express, { Express } from "express";
-import * as http from "http";
-import {
-  APP_HOSTNAME_LOCAL,
-  APP_PORT_CLOUD,
-  APP_PORT_LOCAL,
-  NODE_ENV,
-} from "./config";
-import mongoose, { set } from "mongoose";
-import { DB_CONNECTION_CLOUD, DB_CONNECTION_LOCAL } from "./databases";
-import { Routes } from "./interfaces/routes.interface";
-import helmet from "helmet";
-import cookieParser from "cookie-parser";
-import path = require("path");
-import serverless from "serverless-http";
+import cookieParser from 'cookie-parser'
+import express, { Express } from 'express'
+import helmet from 'helmet'
+import * as http from 'http'
+import mongoose, { set } from 'mongoose'
+import { APP_PORT_CLOUD, APP_PORT_LOCAL, NODE_ENV } from './config'
+import { DB_CONNECTION_CLOUD, DB_CONNECTION_LOCAL } from './databases'
+import { Routes } from './interfaces/routes.interface'
+import path = require('path')
 
 export class App {
-  private _app: Express = express();
-  private _server: http.Server;
-  private _env: string = NODE_ENV || "development";
+  private _app: Express = express()
+  private _server: http.Server
+  private _env: string = NODE_ENV || 'development'
 
   constructor(routes: Routes[]) {
-    this._connectToMongooseDatabase(); // step 1
-    this._createServer(); // step 2
-    this._initializeMiddlewares(); // step 3
-    this._initializeRoutes(routes); // step 4
-
-    // serverless(this._app);
+    this._connectToMongooseDatabase() // step 1
+    this._createServer() // step 2
+    this._initializeMiddlewares() // step 3
+    this._initializeRoutes(routes) // step 4
   }
 
   private _createServer(): void {
-    this._server = http.createServer(this._app);
+    this._server = http.createServer(this._app)
   }
 
   private _connectToMongooseDatabase() {
-    if (this._env === "development") {
-      set("debug", true);
+    if (this._env === 'development') {
+      set('debug', true)
 
-      mongoose
-        .connect(DB_CONNECTION_LOCAL.URL, DB_CONNECTION_LOCAL.OPTIONS)
-        .then((con) => {
-          console.log("Local DB connection successfully !!!");
-        });
+      mongoose.connect(DB_CONNECTION_LOCAL.URL, DB_CONNECTION_LOCAL.OPTIONS).then((con) => {
+        console.log('Local DB connection successfully !!!')
+      })
     }
-    
-    mongoose
-      .connect(DB_CONNECTION_CLOUD.URL, DB_CONNECTION_CLOUD.OPTIONS)
-      .then((con) => {
-        console.log("Cloud DB connection successfully !!!");
-      });
+
+    mongoose.connect(DB_CONNECTION_CLOUD.URL, DB_CONNECTION_CLOUD.OPTIONS).then((con) => {
+      console.log('Cloud DB connection successfully !!!')
+    })
   }
 
   private _initializeMiddlewares(): void {
-    this._app.use(express.static(path.join(__dirname, "assets")));
-    this._app.use(helmet()); // Set security HTTP headers
-    this._app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-    this._app.use(express.json());
-    this._app.use(cookieParser());
+    this._app.use(express.static(path.join(__dirname, 'assets')))
+    this._app.use(helmet()) // Set security HTTP headers
+    this._app.use(express.urlencoded({ extended: true })) // parse application/x-www-form-urlencoded
+    this._app.use(express.json())
+    this._app.use(cookieParser())
   }
 
   private _initializeRoutes(routes: Routes[]): void {
     routes.forEach((route) => {
-      this._app.use("/", route.router);
-    });
+      this._app.use('/', route.router)
+    })
   }
 
   public listenServer(): void {
-    if (this._env === "development") {
+    if (this._env === 'development') {
       this._server.listen(APP_PORT_LOCAL, () => {
-        console.log(`🚀 Server is running on port 👉 ${APP_PORT_LOCAL} 👈`);
-      });
+        console.log(`🚀 Server is running on port 👉 ${APP_PORT_LOCAL} 👈`)
+      })
     } else {
       this._server.listen(APP_PORT_CLOUD, () => {
-        console.log(`🚀 Server is running on port 👉 ${APP_PORT_CLOUD} 👈`);
-      });
+        console.log(`🚀 Server is running on port 👉 ${APP_PORT_CLOUD} 👈`)
+      })
     }
   }
 }
--- BE/shopdienmay/src/routes/product.routes.ts
+++ BE/shopdienmay/src/routes/product.routes.ts
@@ -1,29 +1,28 @@
-import { Router } from "express";
-import ProductController from "../controllers/product.controller";
-import { uploadImages } from "../middlewares/upload-images.middleware"
-import resizeTourimageList from "../middlewares/resize-images.middleware";
-import AuthController from "../controllers/auth.controller";
+import { Router } from 'express'
+import ProductController from '../controllers/product.controller'
+import { uploadImages } from '../middlewares/upload-images.middleware'
+import resizeTourimageList from '../middlewares/resize-images.middleware'
+import AuthController from '../controllers/auth.controller'
 
 class ProductRouter {
-    public path = '/api/v1/products/';
-    public router = Router();
-    public productController = new ProductController();
-    public authController = new AuthController();
+  public path = '/api/v1/products/'
+  public router = Router()
+  public productController = new ProductController()
+  public authController = new AuthController()
 
-    constructor() {
-        this.initializeRoutes();
-    }
+  constructor() {
+    this.initializeRoutes()
+  }
 
-    initializeRoutes() {
-        this.router.get(this.path + ':id', this.productController.getProductById)
-        this.router.patch(this.path + ':id', uploadImages, resizeTourimageList, this.productController.updateProductById);
-        this.router.delete(this.path + ':id', this.productController.delelteProductById)
+  initializeRoutes() {
+    this.router.get(this.path + ':id', this.productController.getProductById)
+    this.router.patch(this.path + ':id', uploadImages, resizeTourimageList, this.productController.updateProductById)
+    this.router.delete(this.path + ':id', this.productController.delelteProductById)
+    this.router.get(this.path, this.productController.getAllProducts)
 
-        this.router.use(this.authController.protect); 
-        this.router.get(this.path, this.productController.getAllProducts)
-        this.router.post(this.path, uploadImages, resizeTourimageList, this.productController.createNewProduct)
-        
-    }
+    this.router.use(this.authController.protect) // Protect route
+    this.router.post(this.path, uploadImages, resizeTourimageList, this.productController.createNewProduct)
+  }
 }
 
-export default ProductRouter;
+export default ProductRouter
