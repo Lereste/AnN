713b51b04ba54a4ca4e42ae0a75fa0a57e223178
Lereste <hohoanghao1999@gmail.com>
2025-06-10 00:51:19 +0700

    [FE] fix: update vercel.json 10

--- FE/projects/admin/src/app/app.config.ts
+++ FE/projects/admin/src/app/app.config.ts
@@ -2,7 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { ADMIN_APP_ROUTES } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(ADMIN_APP_ROUTES), provideClientHydration(withEventReplay())],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(ADMIN_APP_ROUTES),
+    { provide: APP_BASE_HREF, useValue: '/admin' },
+    provideClientHydration(withEventReplay()),
+  ],
 };
