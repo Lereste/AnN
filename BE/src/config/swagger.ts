ec0459a802e14bfed1b9ef2e5ea4b67671809009
Lereste <hohoanghao1999@gmail.com>
2025-06-23 00:40:54 +0700

    [BE] fix: config swagger for document api: categories, reviews, users 2

--- BE/src/config/swagger.ts
+++ BE/src/config/swagger.ts
@@ -2,23 +2,32 @@ import swaggerJSDoc from 'swagger-jsdoc';
 import YAML from 'yamljs';
 import path from 'path';
 
-// Load docs from YAML
+// Load individual Swagger docs
 const productDocs = YAML.load(path.resolve(__dirname, '../docs/products.swagger.yaml'));
 const categoryDocs = YAML.load(path.resolve(__dirname, '../docs/categories.swagger.yaml'));
 const reviewDocs = YAML.load(path.resolve(__dirname, '../docs/reviews.swagger.yaml'));
 const userDocs = YAML.load(path.resolve(__dirname, '../docs/users.swagger.yaml'));
 
+// Merge tags + paths only
 function mergeDocs(...docs: any[]) {
-  // Merge tags and paths from all docs
   const merged = { tags: [], paths: {} };
+  const tagSet = new Set<string>();
+
   for (const doc of docs) {
     if (doc.tags) {
-      merged.tags.push(...doc.tags);
+      for (const tag of doc.tags) {
+        if (!tagSet.has(tag.name)) {
+          tagSet.add(tag.name);
+          merged.tags.push(tag);
+        }
+      }
     }
+
     if (doc.paths) {
       Object.assign(merged.paths, doc.paths);
     }
   }
+
   return merged;
 }
 
@@ -37,10 +46,7 @@ const options: swaggerJSDoc.Options = {
         url: 'https://api-shopdienmay.vercel.app/api/v1',
       },
     ],
-    ...productDocs,
-    ...categoryDocs,
-    ...reviewDocs,
-    ...userDocs,
+    ...mergedDocs,
   },
   apis: ['src/routes/*.ts', 'src/controllers/*.ts', 'src/models/*.ts'],
 };
