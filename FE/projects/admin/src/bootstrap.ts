7096ac1aa1e7e97feee3e4530389e62c180841a9
Lereste <hohoanghao1999@gmail.com>
2025-07-31 00:15:10 +0700

    [FE] config: change angular component to web component 2

--- FE/projects/admin/src/bootstrap.ts
+++ FE/projects/admin/src/bootstrap.ts
@@ -1,25 +1,22 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { createApplication } from '@angular/platform-browser';
 import { createCustomElement } from '@angular/elements';
 import { NgZone } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
+// For micro-frontend setup using Angular Elements
+(async () => {
+  const app = await createApplication({
+    ...appConfig,
+    providers: [
+      ...appConfig.providers,
+      (globalThis as Record<string, unknown>)['ngZone'] ? { provide: NgZone, useValue: (globalThis as Record<string, unknown>)['ngZone'] } : [],
+    ],
+  });
 
+  const AdminRoot = createCustomElement(AppComponent, {
+    injector: app.injector,
+  });
 
-  (async () => {
-    const app = await bootstrapApplication(AppComponent, {
-      ...appConfig,
-      providers: [
-        ...appConfig.providers,
-        (globalThis as Record<string, unknown>)['ngZone'] ? { provide: NgZone, useValue: (globalThis as Record<string, unknown>)['ngZone'] } : [],
-      ],
-    });
-
-    const AdminRoot = createCustomElement(AppComponent, {
-      injector: app.injector,
-    });
-
-    customElements.define('admin-root', AdminRoot);
-  })();
+  customElements.define('admin-root', AdminRoot);
+})();
